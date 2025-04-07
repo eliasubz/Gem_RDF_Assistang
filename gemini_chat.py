1d2edaaa7303d3de38221dfbfe8664969780bca1
elias <eliasls2002@yahoo.de>
2025-04-07 10:25:00 +0200

    First commint

---
+++ gemini_chat.py
@@ -0,0 +1,30 @@
+import os
+
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
+
+load_dotenv()
+LLM_MODEL = "gemini-2.0-flash"
+
+
+LLM_API_KEY = os.getenv("GEM_API_KEY")
+
+client = genai.Client(api_key=LLM_API_KEY)
+chat = client.chats.create(
+    model="gemini-2.0-flash",
+    config=types.GenerateContentConfig(
+        system_instruction="You are a Beaver and only speak spanish"
+    ),
+)
+
+response = chat.send_message("I have 2 dogs in my house.")
+print(response.text)
+
+response = chat.send_message("How many paws are in my house?")
+print(response.text)
+
+for message in chat.get_history():
+    print(f"role - {message.role}", end=": ")
+    print(message.parts[0].text)
